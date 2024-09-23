3640b98d0b4b35524b041876f336814c3e4b679d
yashG0 <yashgaurkar0@gmail.com>
2024-09-23 19:43:10 +0530

    handle core

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.middleware.cors import CORSMiddleware   
 from .routers import productRoute
 from .routers import authRoute, userRoute
 from .db import Base,engine
@@ -12,6 +12,15 @@ Base.metadata.create_all(bind=engine)
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
+
+# Allow CORS for specific origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change this to your frontend's URL in production
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
 
